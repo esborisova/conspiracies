2b59bffaf54165c218bfeaee1a13e19aa98dacfb
kate <sk.borisova@gmail.com>
2022-03-28 12:12:32 +0200

    functions and the application added

---
+++ src/conspiracies/extracting_ents_heads/functions.py
@@ -0,0 +1,79 @@
+
+import spacy
+from collections import Counter
+import pandas as pd
+
+
+def get_headword(noun_phrases: list) -> list:
+  """Extracts headwords with their entity labels from noun phrases
+   Args:
+       noun_phrases (list): A list of lists with strings (noun phrases) 
+   Returns:
+       headwords (list): A list of strings containing headwords and their entity types
+  """ 
+
+  headwords = []
+
+  for phrase in noun_phrases:
+    for span in phrase:
+      for word in span:
+        if word.head.pos_ == "PROPN" or word.head.pos_ == "NOUN" or word.head.pos_ == "PRON":
+          headwords.append(f"{word.head}%%{word.head.ent_type_}") 
+  return headwords
+          
+
+
+def get_entities(noun_phrases: list) -> list:
+  """Extracts entities with their named entity labels from noun phrases
+   Args:
+       noun_phrases (list): A list of lists with strings (noun phrases) 
+   Returns:
+       entaties (list): A list of strings containing entities and their types
+  """ 
+
+  entities = []
+
+  for phrase in noun_phrases:
+    for span in phrase:
+      if span.ents:
+        entities.append(f"{span.ents[0]}%%{span.ents[0][0].ent_type_}")
+  return entities
+
+
+
+def filter_ne_type(ents_heads: dict) -> dict:
+  """Narrows down entities/headwords to the predifiend list of named entity types
+   Args:
+       ents_heads (dict): A dictionary with entities/headwords as keys and their frequencies as values 
+   Returns:
+       new_dict (list): A dictionary containing only entities/headwords (and their freq) belonging to the defined group of entity types 
+  """ 
+
+  ent_types = ['LOC', 'MISC', 'ORG', 'PER']
+
+  new_dict = {}  
+  
+  for tag in ent_types:
+    for key, value in ents_heads.items():
+      if tag in key:
+        new_dict[key] = value
+  return new_dict
+
+
+
+def make_tuples(ranked_ents_heads: dict) -> list:
+  """Creates a list of tuples with: Entity/headword, its named entity type, its frequency
+   Args:
+       ranked_ents_heads (dict): A dictionary with entities/headwords as keys and their frequencies as values 
+   Returns:
+       list_of_tuples (list): A list of tuples containing entities/headwords, their entity type label and frequency 
+  """ 
+  
+  list_of_tuples = []
+  
+  for key, value in ranked_ents_heads.items():
+    splitted_key = key.split("%%")
+    splitted_key.append(value)
+    list_of_tuples.append(tuple(splitted_key))
+   
+  return list_of_tuples
---
+++ src/conspiracies/extracting_ents_heads/pipeline.py
@@ -0,0 +1,67 @@
+from relationextraction import SpacyRelationExtractor
+import spacy
+from collections import Counter
+import pandas as pd
+from functions import*
+
+nlp = spacy.load("da_core_news_lg")
+
+test_sents = [
+    "Pernille Blume vinder delt EM-sølv i Ungarn.",
+    "Pernille Blume blev nummer to ved EM på langbane i disciplinen 50 meter fri.",
+    "Hurtigst var til gengæld hollænderen Ranomi Kromowidjojo, der sikrede sig guldet i tiden 23,97 sekunder.",
+    "Og at formen er til en EM-sølvmedalje tegner godt, siger Pernille Blume med tanke på, at hun få uger siden var smittet med corona.",
+    "Ved EM tirsdag blev det ikke til medalje for den danske medley for mixede hold i 4 x 200 meter fri.",
+    "Politiet skal etterforske Siv Jensen etter mulig smittevernsbrudd.",
+    "En av Belgiens mest framträdande virusexperter har flyttats med sin familj till skyddat boende efter hot från en beväpnad högerextremist.",
+]
+
+
+# change these to your purposes. 2.7 is the default confidence threshold(the bulk of bad relations not kept and the majority of correct ones kept)
+# batch_size should be changed according to your device. Can most likely be bumped up a fair bit
+config = {"confidence_threshold": 2.7, "model_args": {"batch_size": 10}}
+nlp.add_pipe("relation_extractor", config=config)
+
+pipe = nlp.pipe(test_sents)
+
+args = []
+
+#Collect all args1 and args2 
+for d in pipe:
+
+  args.append(d._.relation_head)
+  args.append(d._.relation_tail)
+
+print(args)
+
+#Extract all headwords and their entaty lables from args 
+heads = get_headword(args)
+print(heads)
+
+#Count heads frequency
+heads_freq = Counter(heads)
+print(heads_freq)
+
+#Extract all entaties and their labels from args
+entities = get_entities(args)
+print(entities)
+
+#Count entaties frequency
+entities_freq = Counter(entities)
+print(entities_freq)
+
+#Sum frequencies across heads and entaties
+merged_freq = {k: heads_freq.get(k, 0) + entities_freq.get(k, 0) for k in set(heads_freq) | set(entities_freq)}
+print(merged_freq)
+
+#Filter out enataty types 
+filtered_freq = filter_ne_type(merged_freq)
+print(filtered_freq)
+
+#Rank entaties/heads by frequency 
+ranked_freq = {key: value for key, value in sorted(filtered_freq.items(), key=lambda item: item[1],reverse=True)}
+print(ranked_freq)
+
+#Create a list of tuples with ents/heads, type tag, freq 
+ents_tuples = make_tuples(ranked_freq)
+print(ents_tuples)
