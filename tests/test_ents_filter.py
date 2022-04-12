c4b47b8d20d48fcfed2518732c08fcbf0b7a2c1b
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-04-12 15:22:00 +0200

    removed english pipeline from tests

--- tests/test_ents_filter.py
+++ tests/test_ents_filter.py
@@ -1,13 +1,14 @@
 import spacy
+from spacy.tokens import Doc, Span
 from conspiracies.component_HeadWordExtraction import contains_ents
 
 
 def test_ents_filter():
-
-    nlp = spacy.load("en_core_web_lg")
+    nlp = spacy.blank("en")
     nlp.add_pipe("heads_extraction")
+    doc = Doc(nlp.vocab, words="Mette Frederiksen is the Danish Politician".split())
+    doc.set_ents([Span(doc, 0, 2, "PERSON")])
 
-    doc = nlp("Mette Frederiksen is the Danish politician.")
     assert contains_ents(doc[:]) is True
     assert contains_ents(doc[1:2]) is True
     assert contains_ents(doc[2:4]) is False
