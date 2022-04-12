a2528cbbacc10cae3806676e8a1b5025c3067384
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-04-12 17:29:58 +0200

    added main scope

--- src/application/ents_heads_extraction.py
+++ src/application/ents_heads_extraction.py
@@ -1,39 +1,42 @@
 """
 Pipeline for headwords/entities extractions and frequency count
 """
-
-from relationextraction import SpacyRelationExtractor
-from heads_extract_component import HeadwordsExtraction, contains_ents
 import spacy
-from spacy.tokens import Span
-from collections import Counter
+
+from relationextraction import SpacyRelationExtractor  # noqa
+from conspiracies.component_HeadWordExtraction import contains_ents
 
 
-nlp = spacy.load("en_core_web_lg")
+def main()
+    nlp = spacy.load("en_core_web_lg")
 
-test_sents = ["Mette Frederiksen is the Danish politician."]
+    test_sents = ["Mette Frederiksen is the Danish politician."]
 
-config = {"confidence_threshold": 2.7, "model_args": {"batch_size": 10}}
-nlp.add_pipe("relation_extractor", config=config)
-nlp.add_pipe("heads_extraction")
+    config = {"confidence_threshold": 2.7, "model_args": {"batch_size": 10}}
+    nlp.add_pipe("relation_extractor", config=config)
+    nlp.add_pipe("heads_extraction")
 
-pipe = nlp.pipe(test_sents)
+    pipe = nlp.pipe(test_sents)
 
-heads_spans = []
-ents_spans = []
+    heads_spans = []
+    ents_spans = []
 
-for d in pipe:
-    for span in d._.relation_head:
-        heads_spans.append(span._.most_common_ancestor)
-        if span.ents:
-            ents_spans.append(list(span.ents))
-    for span in d._.relation_tail:
-        heads_spans.append(span._.most_common_ancestor)
-        if span.ents:
-            ents_spans.append(list(span.ents))
+    for d in pipe:
+        for span in d._.relation_head:
+            heads_spans.append(span._.most_common_ancestor)
+            if span.ents:
+                ents_spans.append(list(span.ents))
+        for span in d._.relation_tail:
+            heads_spans.append(span._.most_common_ancestor)
+            if span.ents:
+                ents_spans.append(list(span.ents))
 
-# Filter out headwords without an entity type
-filtered_heads = list(filter(contains_ents, heads_spans))
+    # Filter out headwords without an entity type
+    filtered_heads = list(filter(contains_ents, heads_spans))
 
-# Flatten the list with ents
-flat_ents_list = [ent for sublist in ents_spans for ent in sublist]
+    # Flatten the list with ents
+    flat_ents_list = [ent for sublist in ents_spans for ent in sublist]
+
+
+if __name__ == "__main__":
+    main()
