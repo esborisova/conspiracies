40bfb2ba01ceb5dd2f600e6ede2606c092e04e46
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-04-12 17:50:01 +0200

    removed relation extraction from tests

--- tests/test_head_extractor.py
+++ tests/test_head_extractor.py
@@ -3,22 +3,11 @@ from .utils import nlp_en  # noqa
 from spacy.tokens import Span
 
 import conspiracies  # noqa
-from relationextraction import SpacyRelationExtractor  # noqa
 
 
-def test_head_extractor(nlp_en):
-
-    test_sents = ["Mette Frederiksen is the Danish politician."]
-    config = {"confidence_threshold": 2.7, "model_args": {"batch_size": 10}}
-
-    nlp_en.add_pipe("relation_extractor", config=config)
+def test_most_common_ancestor(nlp_en):
     nlp_en.add_pipe("heads_extraction")
+    doc = nlp_en("Mette Frederiksen is the Danish politician.")
 
-    pipe = nlp_en.pipe(test_sents)
-
-    for d in pipe:
-        for span in d._.relation_head:
-            headword = span._.most_common_ancestor
-
-            assert isinstance(headword, Span)
-            assert headword.text == "Frederiksen"
+    assert doc[0:2]._.most_common_ancestor.text == "Frederiksen"  # Span
+    assert doc._.most_common_ancestor.text == "is"  # Doc
