0f172987680c0e34172c66226b6c8f818ba351e6
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-04-19 14:14:15 +0200

    split test in two

--- tests/test_coref_component.py
+++ tests/test_coref_component.py
@@ -5,7 +5,7 @@ from .utils import nlp_da_w_coref  # noqa
 from conspiracies.coref import CoreferenceComponent  # noqa
 
 
-def test_coref_component(nlp_da_w_coref):
+def test_coref_clusters(nlp_da_w_coref):
 
     text = (
         "Aftalepartierne bag Rammeaftalen om plan for genåbning af Danmark blev i"
@@ -14,6 +14,15 @@ def test_coref_component(nlp_da_w_coref):
         + "coronaepidemien i Danmark. Ekspertgruppen er nu klar med sin rapport."
     )
 
+    doc = nlp_da_w_coref(text)
+    # test attributes is set as intended
+    assert isinstance(doc._.coref_clusters, list)
+    for sent in doc.sents:
+        assert isinstance(sent._.coref_clusters, list)
+        assert isinstance(sent._.coref_clusters[0][1], Span)
+
+
+def test_resolve_coref(nlp_da_w_coref):
     resolve_coref_text = (
         "Aftalepartierne bag Rammeaftalen om plan for genåbning af Danmark blev i"
         + " foråret 2021 enige om at nedsætte en ekspertgruppe, en ekspertgruppe fik til opgave at "
@@ -27,14 +36,11 @@ def test_coref_component(nlp_da_w_coref):
         "en ekspertgruppe er nu klar med en ekspertgruppe rapport.",
     ]
 
-    doc = nlp_da_w_coref(text)
-    # test attributes is set a
-    assert isinstance(doc._.coref_clusters, list)
+    doc = nlp_da_w_coref(resolve_coref_text)
+    # test for doc
     assert doc._.resolve_coref == resolve_coref_text
+
+    # test for spans
     for i, sent in enumerate(doc.sents):
-        assert isinstance(sent._.coref_clusters, list)
         if sent._.coref_clusters != []:
             assert sent._.resolve_coref == resolve_coref_spans[i]
-            assert isinstance(sent._.coref_clusters[0], tuple)
-            assert isinstance(sent._.coref_clusters[0][0], int)
-            assert isinstance(sent._.coref_clusters[0][1], Span)
