6bf715af48e4e4bd974962654718fb6efdb8cd04
kate <sk.borisova@gmail.com>
2022-09-12 13:17:47 +0200

    draft for supernodes collection step

---
+++ src/application/collecting_supernodes.py
@@ -0,0 +1,54 @@
+import nltk
+from nltk.tokenize import word_tokenize
+import pandas as pd
+from operator import itemgetter
+from supernodes import preprocess_seeds, find_most_freq_substring
+
+stemmer = nltk.stem.SnowballStemmer('english')
+
+df = pd.read_csv("Bridgegate.csv")
+
+entities = df["rank"].tolist()
+tokens = [word_tokenize(entity) for entity in entities]
+stems = [list(map(lambda token: stemmer.stem(token), list_of_tokens)) for list_of_tokens in tokens]
+joined_stems = [" ".join(stem) for stem in stems]
+df["stems"] = joined_stems
+
+current_list = df.values.tolist()
+sorted_current_list = sorted(current_list, key=itemgetter(2), reverse=True)
+
+supernodes = []
+max_seeds = 4
+
+while len(sorted_current_list)>0:
+
+    seeds = []
+   
+    seeds.append(sorted_current_list[0][0])
+    sorted_current_list.pop(0) 
+
+    seeds_stems = preprocess_seeds(seeds, stemmer)
+    substrings = [entity for seed in seeds_stems for entity in sorted_current_list if seed in entity[3]]
+
+    if len(substrings) != 0:
+        
+        most_freq_substring = find_most_freq_substring(substrings) 
+
+        for item in reversed(sorted_current_list):
+            if item == most_freq_substring: 
+                seeds.append(most_freq_substring[0])
+                sorted_current_list.pop(sorted_current_list.index(most_freq_substring))
+
+                #if len(seeds) == max_seeds:
+                #    supernodes.append(seeds)
+                #    return to line 23 (this supernode is complere. Start collecting seeds for a new supernode)
+                #else:
+                #    jump to line 31 (continue collecting substrings)       
+                
+            else:
+                supernodes.append(seeds)
+                #start from line 23 again (new supernode)
+
+    else:
+        supernodes.append(seeds) 
+        #start from line 23 again (new supernode)     
---
+++ src/conspiracies/supernodes.py
@@ -0,0 +1,20 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from operator import itemgetter
+from typing import List
+
+
+def preprocess_seeds(seeds_list: List[str], stemmer) -> List[str]:
+
+    seeds_tokens = [word_tokenize(seed) for seed in seeds_list]
+    seeds_stems = [list(map(lambda token: stemmer.stem(token), list_of_tokens)) for list_of_tokens in seeds_tokens]
+    joined_seeds_stems = [" ".join(seed) for seed in seeds_stems]
+
+    return joined_seeds_stems 
+
+
+def find_most_freq_substring(list_of_substrings: List[List[str, str, int]]) -> List[str, str, int]:
+
+    sorted_list = sorted(list_of_substrings, key=itemgetter(2), reverse=True)
+
+    return sorted_list[0]
