65abd177620201c63d75ebd11970562fd3041f1b
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2022-09-16 13:46:02 +0200

    colaborative coding

--- src/application/collecting_supernodes.py
+++ src/application/collecting_supernodes.py
@@ -1,8 +1,6 @@
 import nltk
-from nltk.tokenize import word_tokenize
 import pandas as pd
-from operator import itemgetter
-from supernodes import preprocess_seeds, find_most_freq_substring
+from nltk.tokenize import word_tokenize
 
 stemmer = nltk.stem.SnowballStemmer("english")
 
@@ -10,53 +8,64 @@ df = pd.read_csv("Bridgegate.csv")
 
 entities = df["rank"].tolist()
 tokens = [word_tokenize(entity) for entity in entities]
-stems = [
-    list(map(lambda token: stemmer.stem(token), list_of_tokens))
-    for list_of_tokens in tokens
-]
+stems = [list(map(stemmer.stem, list_of_tokens)) for list_of_tokens in tokens]
 joined_stems = [" ".join(stem) for stem in stems]
-df["stems"] = joined_stems
 
-current_list = df.values.tolist()
-sorted_current_list = sorted(current_list, key=itemgetter(2), reverse=True)
+head_and_entities = sorted(df.values.tolist(), key=lambda x: x[2])
 
+# Step-0: The current entity/concept list is set equal to the original list.
+# The maximum number of seed nodes in asupernode isset to k.
+current_list = head_and_entities  # TODO: remove if irrelevant or add copy
+phrases = current_list  # TODO replace with actual phrases
 supernodes = []
 max_seeds = 4
 
-while len(sorted_current_list) > 0:
+S_seed_nodes = set()
+while len(current_list) > 0:
 
-    seeds = []
+    # Step-1: If the current list is empty, then Quit (supernode construction
+    # iscomplete). Otherwise, select the highest ranked entity/concept in the current
+    # list (in the first iteration, the entire original list isthe current list). Let
+    # this entity be E_1. Add E_1 to the list of seed nodes for the new supernode, S.
+    # Remove E_1 from the current list. Set the seed-node list size, |S|=1.
+    if not S_seed_nodes:
+        e_1 = current_list.pop()[0]
+        S_seed_nodes.add(e_1)
 
-    seeds.append(sorted_current_list[0][0])
-    sorted_current_list.pop(0)
-
-    seeds_stems = preprocess_seeds(seeds, stemmer)
-    substrings = [
-        entity
-        for seed in seeds_stems
-        for entity in sorted_current_list
-        if seed in entity[3]
+    # Step-2: Find all phrases/arguments where any of the seed nodes in the
+    # set S (i.e. the set representing the supernode under construction) appears as a
+    # sub-string, and let this be called P.
+    phrases_w_seed = [
+        (phrase, freq)
+        for phrase, tag, freq in phrases
+        if any(seed in phrase for seed in S_seed_nodes)
     ]
 
-    if len(substrings) != 0:
-
-        most_freq_substring = find_most_freq_substring(substrings)
-
-        for item in reversed(sorted_current_list):
-            if item == most_freq_substring:
-                seeds.append(most_freq_substring[0])
-                sorted_current_list.pop(sorted_current_list.index(most_freq_substring))
-
-                # if len(seeds) == max_seeds:
-                #    supernodes.append(seeds)
-                #    return to line 23 (this supernode is complere. Start collecting seeds for a new supernode)
-                # else:
-                #    jump to line 31 (continue collecting substrings)
-
-            else:
-                supernodes.append(seeds)
-                # start from line 23 again (new supernode)
-
+    # Step-3: Compute the most frequent entity/concept in the original list (other than
+    # the seed nodes already extracted) in P. Let this be E.
+    if phrases_w_seed:
+        E = max(phrases_w_seed, key=lambda x: x[1])[0]
     else:
-        supernodes.append(seeds)
-        # start from line 23 again (new supernode)
+        # Step-4: If E has been processed before (i.e., it is no longer in the current
+        # list), then jump to Step-6.
+        # Step-6: The current list of seed nodes, S, is the new supernode. Return to
+        # Step-1 to start creating a new supernode.
+        supernodes.append(S_seed_nodes)
+        S_seed_nodes = set()
+        continue
+
+    # Step-5: If E is in the current list, then add it to the list of seed nodes, S.
+    # Remove it from the current list of entities/concepts. Increase the size count,
+    # |S| =|S|+ 1. If |S| = k (where k is the maximum size of the supernode seed list
+    # S), then go to Step-6.
+    S_seed_nodes.add(E)
+    current_list = [entity for entity in current_list if entity[0] != E]
+
+    if len(S_seed_nodes) == max_seeds:
+        # Step-6: The current list of seed nodes, S, is the new supernode. Return to
+        # Step-1 to start creating a new supernode.
+        supernodes.append(S_seed_nodes)
+        S_seed_nodes = set()
+        continue
+
+    # Otherwise jump to Step-2
--- src/conspiracies/supernodes.py
+++ src/conspiracies/supernodes.py
@@ -1,15 +1,14 @@
-import nltk
-from nltk.tokenize import word_tokenize
 from operator import itemgetter
-from typing import List
+from typing import List, Tuple
+
+from nltk.tokenize import word_tokenize
 
 
 def preprocess_seeds(seeds_list: List[str], stemmer) -> List[str]:
 
     seeds_tokens = [word_tokenize(seed) for seed in seeds_list]
     seeds_stems = [
-        list(map(lambda token: stemmer.stem(token), list_of_tokens))
-        for list_of_tokens in seeds_tokens
+        list(map(stemmer.stem, list_of_tokens)) for list_of_tokens in seeds_tokens
     ]
     joined_seeds_stems = [" ".join(seed) for seed in seeds_stems]
 
@@ -17,8 +16,8 @@ def preprocess_seeds(seeds_list: List[str], stemmer) -> List[str]:
 
 
 def find_most_freq_substring(
-    list_of_substrings: List[List[str, str, int]]
-) -> List[str, str, int]:
+    list_of_substrings: List[Tuple[str, str, int]]
+) -> Tuple[str, str, int]:
 
     sorted_list = sorted(list_of_substrings, key=itemgetter(2), reverse=True)
 
