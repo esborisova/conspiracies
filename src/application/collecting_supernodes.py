94263760d9deae832bfe97dc698f2d457964b33e
kate <sk.borisova@gmail.com>
2022-09-19 12:15:17 +0200

    remove stemming

--- src/application/collecting_supernodes.py
+++ src/application/collecting_supernodes.py
@@ -18,8 +18,14 @@ while len(heads_and_entities) > 0:
         seed = heads_and_entities.pop()[0]
         set_of_seeds.add(seed)
 
-    phrases_with_seed = [(phrase, freq) for phrase, tag, freq in heads_and_entities if any(lemmatize(seed, nlp=nlp) in lemmatize(phrase, nlp=nlp) for seed in set_of_seeds)]
-   
+    phrases_with_seed = [
+        (phrase, freq)
+        for phrase, tag, freq in heads_and_entities
+        if any(
+            lemmatize(seed, nlp=nlp) in lemmatize(phrase, nlp=nlp)
+            for seed in set_of_seeds
+        )
+    ]
 
     if phrases_with_seed:
         most_freq_substr = max(phrases_with_seed, key=lambda x: x[1])[0]
@@ -28,9 +34,10 @@ while len(heads_and_entities) > 0:
         set_of_seeds = set()
         continue
 
-
     set_of_seeds.add(most_freq_substr)
-    heads_and_entities = [entity for entity in heads_and_entities if entity[0] != most_freq_substr]
+    heads_and_entities = [
+        entity for entity in heads_and_entities if entity[0] != most_freq_substr
+    ]
 
     if len(set_of_seeds) == max_seeds:
 
--- src/conspiracies/supernodes.py
+++ src/conspiracies/supernodes.py
@@ -1,6 +1,4 @@
-import nltk
-from nltk.tokenize import word_tokenize
-
+import spacy
 
 def lemmatize(doc: str, nlp) -> str:
     """
@@ -15,20 +13,3 @@ def lemmatize(doc: str, nlp) -> str:
     docs = nlp(doc)
     lemmas = " ".join([token.lemma_ for token in docs])
     return lemmas
-
-
-def stem(doc: str, language="english") -> str:
-    """
-    Stemms a sequence of words.
-    Args:
-        doc (str): A sequence to be stemmed.
-        language: The language of a doc under stemming.
-    Returns:
-        str: Stemmed word sequence.
-    """
-
-    stemmer = nltk.stem.SnowballStemmer(language)
-    tokens = word_tokenize(doc)
-    stemms = [stemmer.stem(token) for token in tokens]
-    stemms = " ".join(stemms)
-    return stemms
